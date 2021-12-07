1967678c29cd69387ae06cddfc3c56c37a45b2f1
Felipe Petrocelli <95191209+felipepetrocelli@users.noreply.github.com>
2021-12-07 16:53:37 -0300

    Update routes.js

--- api_node/src/routes.js
+++ api_node/src/routes.js
@@ -4,9 +4,9 @@ const ClienteController = require('./controller/ClienteController');
 const routes = express.Router();
 
 //ROTAS CLIENTES
+routes.post('/api/clientes', ClienteController.store)
 routes.get('/api/clientes', ClienteController.index)
 routes.get('/api/clientes/:id', ClienteController.findByid)
-routes.post('/api/clientes', ClienteController.store)
 routes.delete('/api/clientes/:id', ClienteController.delete)
 routes.put('/api/clientes/:id', ClienteController.update)
 
