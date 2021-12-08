21a29b32fb197bbdcf1814e892a4da912f2dcddd
Felipe Petrocelli <95191209+felipepetrocelli@users.noreply.github.com>
2021-12-08 09:25:52 -0300

    server-commit

--- frontAngular/src/app/app.module.ts
+++ frontAngular/src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ClientesCrudComponent } from './views/clientes-crud/clientes-crud.component';
 import { ClienteCreateComponent } from './components/clieente/cliente-create/cliente-create.component'
 import { MatButtonModule } from '@angular/material/button';
-import  {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
